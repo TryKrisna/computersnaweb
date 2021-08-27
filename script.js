1fbf73cc3188d699ee8a7b07272ba3bb35c94b44
TryKrisna <88521728+TryKrisna@users.noreply.github.com>
2021-08-27 10:54:49 +0700

    start of the day
    san computer web

---
+++ script.js
@@ -0,0 +1,135 @@
+
+window.onload = function(){ 
+  alert("This is make for Study Only. @Try Krisna");
+  }
+// let menu = document.querySelector('#menu-bars');
+// let navbar = document.querySelector('.navbar');
+
+// menu.onclick = () =>{
+//   // menu.classList.toggle('fa-times');
+//   navbar.classList.toggle('active');
+// }
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  // menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () =>{
+
+  // menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+
+  });
+
+}
+// End menu
+// htmlslide
+
+// Slideeer
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";s
+}
+
+// End slider
+
+
+// end slide 
+
+
+
+
+
+
+
+
+
+// Slider
+// var swiper = new Swiper(".top_slider", {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 7500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   // navigation: {
+//   //   nextEl: ".swiper-button-next",
+//   //   prevEl: ".swiper-button-prev",
+//   // },
+//   loop:true,
+// });
+
+
+
+
+// End slider
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+  loop:true,
+});
+
+// review slider 
+
+// end slider
+
